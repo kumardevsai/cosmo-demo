3cac51f55a61e03de23e4e36aafc452668127142
huanghaiyang <huanhai24@sina.com>
2013-11-19 18:41:34 +0800

    update

---
+++ fpd/grid-collapse.js
@@ -0,0 +1,226 @@
+// 折叠
+function pucker() {
+	var tb_div = document.getElementById('table0');
+	var tb = tb_div.getElementsByTagName('table')[0];
+	var tbody = tb.tBodies[0];
+	var mergeCols = attr(tbody, 'MergeCols').split(',');
+	var arr = chargeArrayProperty(mergeCols);
+	var tHead = tb.tHead;
+	for (var i = 0; i < tHead.children.length - 1; i++) {
+		for (var j = 0; j < tHead.children[i].cells.length; j++) {
+			var cell = tHead.children[i].cells[j];
+			if (!arr[attr(cell, 'row')])
+				cell.innerHTML = '<img src="images/elbow-minus-nl.gif" style="vertical-align:middle;" collapse="false" onclick="javascript:pucker_img(this , 0)">' + cell.innerHTML;
+		}
+	}
+	var max_row = parseInt(mergeCols.pop());
+	for (var i = 0; i < tbody.children.length; i++) {
+		for (var j = 0; j <= max_row - 1; j++) {
+			var cell = tbody.children[i].cells[j];
+			if (attr(cell, 'row') < max_row)
+				cell.innerHTML = '<img src="images/elbow-minus-nl.gif" style="vertical-align:middle;" collapse="false" onclick="javascript:pucker_img(this , 1)">' + cell.innerHTML;
+		}
+	}
+}
+
+
+function pucker_horizontal(cell, collapse) {
+	var rowSpan = parseInt(cell.rowSpan, 10);
+	var col_num = parseInt(attr(cell, 'col'));
+	var row_num = parseInt(attr(cell, 'row'));
+	var tbody = cell.parentNode.parentNode;
+	var tb = tbody.parentNode;
+	var row_index = cell.parentNode.rowIndex;
+	var tHead_num = tb.tHead.children.length;
+	if (collapse === true) {
+		// 右单元格操作
+		for (var i = row_index; i < rowSpan + col_num; i++) {
+			var row = tbody.children[i - tHead_num];
+			if (i === row_index) {
+				for (var j = 0; j < row.cells.length; j++) {
+					var cell_ = row.cells[j];
+					if (parseInt(attr(cell_, 'row')) === row_num + 1 && col_num == attr(cell_, 'col')) {
+						cell.rowSpan = parseInt(cell_.rowSpan);
+						break;
+					}
+				}
+			} else if (i > row_index + parseInt(cell.rowSpan) - 1) {
+				if (attr(cell, 'collapse_rows') === null)
+					attr(cell, 'collapse_rows', i - tHead_num);
+				else
+					attr(cell, 'collapse_rows', attr(cell, 'collapse_rows') + ',' + (i - tHead_num));
+				row.style.display = 'none';
+			}
+		}
+		// 左单元格操作
+		for(var i = 0 ; i < tbody.children.length ; i ++)
+		{
+			for(var j = 0 ; j < tbody.children[i].cells.length ; j ++)
+			{
+				var cell_ = tbody.children[i].cells[j];
+			}
+		}
+	} else {
+		var collapse_rows = attr(cell, 'collapse_rows');
+		collapse_rows = collapse_rows.split(',');
+		for (var i = 0; i < collapse_rows.length; i++) {
+			tbody.children[collapse_rows[i]].style.display = '';
+		}
+		cell.rowSpan = parseInt(cell.rowSpan) + collapse_rows.length;
+		cell.removeAttribute('collapse_rows');
+	}
+}
+
+function pucker_vertical(cell, collapse) {
+	var colSpan = parseInt(cell.colSpan, 10);
+	var row_number = parseInt(attr(cell, 'row'));
+	var begin_row = row_number;
+	var thead = cell.parentNode.parentNode;
+	var tb = thead.parentNode;
+	if (collapse === true) {
+		// 下表头收缩
+		for (var i = cell.parentNode.rowIndex + 1; i < thead.children.length; i++) {
+			var row = thead.children[i];
+			for (var j = 0; j < row.cells.length; j++) {
+				var cell_ = row.cells[j];
+				var cell_row = parseInt(attr(cell_, 'row'));
+				if (cell_row === row_number && i === cell.parentNode.rowIndex + 1) {
+					if (!attr(cell, 'factColSpan'))
+						attr(cell, 'factColSpan', cell.colSpan);
+					cell.colSpan = cell_.colSpan;
+					begin_row = parseInt(cell_.colSpan);
+					continue;
+				} else if (cell_row >= row_number + begin_row && (cell_row < colSpan + row_number || cell_row < parseInt(attr(cell, 'factColSpan')) + row_number)) {
+					if (cell_.style.display !== 'none') {
+						if (attr(cell, 'collapse_hcells') === null)
+							attr(cell, 'collapse_hcells', i + '_' + cell_row);
+						else
+							attr(cell, 'collapse_hcells', attr(cell, 'collapse_hcells') + ',' + i + '_' + cell_row);
+						cell_.style.display = 'none';
+					}
+				}
+			}
+		}
+		// 上表头收缩
+		for (var i = 0; i < cell.parentNode.rowIndex; i++) {
+			var row = thead.children[i];
+			for (var j = 0; j < row.cells.length; j++) {
+				var cell_ = row.cells[j];
+				var cell_row = parseInt(attr(cell_, 'row'));
+				if (cell_row + parseInt(cell_.colSpan) >= row_number + colSpan || cell_row + parseInt(attr(cell_, 'factColSpan')) >= row_number + colSpan) {
+					if (!attr(cell_, 'factColSpan'))
+						attr(cell_, 'factColSpan', cell_.colSpan);
+					cell_.colSpan = cell_.colSpan - (colSpan - parseInt(cell.colSpan));
+					break;
+				}
+			}
+		}
+		// 单元格
+		for (var i = 0; i < tb.tBodies[0].children.length; i++) {
+			var row = tb.tBodies[0].children[i];
+			for (var j = 0; j < row.cells.length; j++) {
+				var cell_ = row.cells[j];
+				var cell_row = parseInt(attr(cell_, 'row'));
+				if (cell_row === row_number && i === cell.parentNode.rowIndex + 1) {
+					continue;
+				} else if (cell_row >= row_number + begin_row && (cell_row < colSpan + row_number || cell_row < parseInt(attr(cell, 'factColSpan')) + row_number)) {
+					if (cell_.style.display !== 'none') {
+						if (attr(cell, 'collapse_bcells') === null)
+							attr(cell, 'collapse_bcells', i + '_' + cell_row);
+						else
+							attr(cell, 'collapse_bcells', attr(cell, 'collapse_bcells') + ',' + i + '_' + cell_row);
+						cell_.style.display = 'none';
+					}
+				}
+			}
+		}
+	} else if (collapse === false) {
+		// 获取隐藏的表头
+		var collapse_hcells = attr(cell, 'collapse_hcells');
+		if (!stringAvialable(collapse_hcells))
+			return;
+		var arrh = chargeArrayProperty(collapse_hcells.split(','));
+		// 获取隐藏的单元格
+		var collapse_bcells = attr(cell, 'collapse_bcells');
+		if (!stringAvialable(collapse_bcells))
+			return;
+		var arrb = chargeArrayProperty(collapse_bcells.split(','));
+		collapse_bcells = collapse_bcells.split(',');
+		var factColSpan = parseInt(attr(cell, 'factColSpan'));
+		// 上表头设置跨行属性
+		for (var i = 0; i < cell.parentNode.rowIndex; i++) {
+			var row = thead.children[i];
+			for (var j = 0; j < row.cells.length; j++) {
+				var cell_ = row.cells[j];
+				var cell_row = parseInt(attr(cell_, 'row'));
+				if (cell_row + parseInt(cell_.colSpan) >= row_number + colSpan || cell_row + parseInt(attr(cell_, 'factColSpan')) >= row_number + colSpan) {
+					cell_.colSpan = cell_.colSpan + factColSpan - 1;
+					break;
+				}
+			}
+		}
+		var num = 0;
+		// 下表头展开
+		for (var i = cell.parentNode.rowIndex + 1; i < thead.children.length; i++) {
+			var row = thead.children[i];
+			for (var j = 0; j < row.cells.length; j++) {
+				var cell_ = row.cells[j];
+				var cell_row = parseInt(attr(cell_, 'row'));
+				if (i === cell.parentNode.rowIndex + 1) {
+					if (cell_row >= row_number && (cell_row < colSpan + row_number || cell_row < parseInt(attr(cell, 'factColSpan')) + row_number))
+						num += cell_.colSpan;
+				}
+				if (arrh[i + '_' + cell_row] !== '' && arrh[i + '_' + cell_row] !== undefined)
+					cell_.style.display = 'block';
+			}
+		}
+		cell.colSpan = num;
+		// 单元格展开
+		for (var i = 0; i < tb.tBodies[0].children.length; i++) {
+			var row = tb.tBodies[0].children[i];
+			for (var j = 0; j < row.cells.length; j++) {
+				var cell_ = row.cells[j];
+				var cell_row = parseInt(attr(cell_, 'row'));
+				if (arrb[i + '_' + cell_row] !== '' && arrb[i + '_' + cell_row] !== undefined)
+					cell_.style.display = 'block';
+			}
+		}
+		cell.removeAttribute('collapse_hcells');
+		cell.removeAttribute('collapse_bcells');
+	}
+}
+
+function pucker_img(img, flag) {
+	var cell = img.parentNode;
+	if (attr(img, 'collapse') === 'false') {
+		if (flag === 0)
+			pucker_vertical(cell, true);
+		else
+			pucker_horizontal(cell, true);
+		attr(img, 'collapse', true);
+		img.src = 'images/elbow-end-plus-nl.gif';
+	} else {
+		if (flag === 0)
+			pucker_vertical(cell, false);
+		else
+			pucker_horizontal(cell, false);
+		attr(img, 'collapse', false);
+		img.src = 'images/elbow-minus-nl.gif';
+	}
+}
+
+function chargeArrayProperty(array) {
+	if (Object.prototype.toString.call(array) === '[object Array]') {
+		var arr = [];
+		for (var i in array) {
+			if (array.hasOwnProperty(i))
+				arr[array[i]] = array[i];
+		}
+		return arr;
+	} else
+		return null;
+}
+
+function stringAvialable(value) {
+	return value !== undefined && value !== '' && value !== null;
+}
