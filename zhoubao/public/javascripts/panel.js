e2ea6b41ac391948fb6f92af177469d4c374dbc7
huanghaiyang <huanhai24@sina.com>
2014-07-18 17:58:36 +0800

    update

--- zhoubao/public/javascripts/panel.js
+++ zhoubao/public/javascripts/panel.js
@@ -1,5 +1,5 @@
 // 标签面板
-var panel = (function(win) {
+var tabPanelGroup = (function(win) {
 
 	// 文档
 	var doc = win.document;
@@ -8,13 +8,28 @@ var panel = (function(win) {
 	var tabParent;
 
 	// 标签集合
-	var tabs = {};
+	var tabs = {
+		collection: {},
+		activedId: null,
+		activeOne: function(tab) {
+			if (this.activedId)
+				this.disActiveOne(this.collection[this.activedId]);
 
-	// 面板父元素
-	var panelParent;
-
-	// 面板集合
-	var panels = {};
+			tab.active();
+			this.activedId = tab.id;
+		},
+		disActiveOne: function(tab) {
+			tab.disActive();
+			this.activedId = null;
+		},
+		destroyOne: function(tab) {
+			tab.destroy();
+			if (this.activedId === tab.id) {
+				this.activedId = null;
+				this.collection[tab.id] = null;
+			}
+		}
+	};
 
 	// 标签类型
 	var Types = {
@@ -22,49 +37,161 @@ var panel = (function(win) {
 		holder: "holder"
 	};
 
-	// 模板
-	var templates = {
-		tab: '<li><a href="javascript:openPanel()">{text}</a></li>'
+	function Tab(obj) {
+		this.id = "";
+		this.panel = null;
+		this.type = "";
+		this.text = "";
+		this.title = "";
+		this.closable = true;
+		this.actived = false;
+
+		if (obj) {
+			this.id = obj.id;
+			this.type = obj.type ? obj.type : "";
+			this.text = obj.text ? obj.text : "";
+			this.title = obj.title ? obj.title : this.text;
+			this.closable = obj.closable !== undefined ? obj.closable : true;
+		}
 	};
 
-	// 创建一个新的标签面板
-	/**
-		{
-			id : String,
-			text : String,
-			type : String		
-		}
-	**/
-	function createNewPanel(obj) {
-		var type = obj.type;
-		switch (type) {
-			case Types.holder:
+	Tab.prototype = (function() {
+		return {
+			active: function() {
+				this.element.classList.add("active");
+				this.actived = true;
 
-				break;
-			default:
-				break;
-		}
+				if (this.panel) {
+					this.panel.show();
+				}
+			},
+			disActive: function() {
+				this.element.classList.remove("active");
+				this.actived = false;
+
+				if (this.panel) {
+					this.panel.hide();
+				}
+			},
+			on: function(name, handler, flag) {
+				if (handler) {
+					switch (name) {
+						case "active":
+							utils.AttachEvent(this.element, "click", handler, flag);
+							break;
+						case "close":
+							utils.AttachEvent(this.element.children[0].children[0], "click", handler, flag);
+							break;
+						default:
+							break;
+					}
+				}
+			},
+			destroy: function() {
+				this.element.parentNode.removeChild(this.element);
+				if (this.panel)
+					this.panel.element.parentNode.removeChild(this.panel.element);
+			}
+		};
+	}());
+
+	function Panel() {
+		this.tab = null;
+		this.element = null;
+		this.showed = false;
+	};
+
+	Panel.prototype = (function() {
+		return {
+			show: function() {
+				this.element.style.display = "";
+				this.showed = true;
+			},
+			hide: function() {
+				this.element.style.display = "none";
+				this.showed = false;
+			},
+			destroy: function() {
+				this.element.parentNode.removeChild(this.element);
+			}
+		};
+	}());
+
+	// 模板
+	var templates = {
+		tab: '<li><a href="javascript:return false;" title="{title}">{text}{?<i class="glyphicon glyphicon-remove" style="margin-left:4px;"></i>?}</a></li>',
+		panel: '<div class="panel panel-default" style="border-top:none;display:none;"><div class="panel-body"></div></div>'
+	};
+
+	/**
+		@desciption 创建一个新的标签面板
+		@param obj {id : String,text : String,type : String}
+	**/
+	function createNewPanel(obj, callback) {
+		var tab = new Tab({
+			id: "tab_" + obj.id,
+			type: obj.type,
+			text: obj.text,
+			closable: obj.closable
+		});
+		var tabElement = createTab(obj);
+		tabParent.appendChild(tabElement);
+		tab.element = tabElement;
+		tab.on("active", function() {
+			if (tabs.activedId !== tab.id) {
+				tabs.activeOne(tab);
+			}
+		}, false);
+		if (tab.closable === true)
+			tab.on("close", function() {
+				tabs.destroyOne(tab);
+			}, false);
+		tabs.collection[tab.id] = tab;
+
+		var panel = new Panel();
+		var panelElement = createPanel();
+		panelParent.appendChild(panelElement);
+		panel.element = panelElement;
+
+		tab.panel = panel;
+		panel.tab = tab;
+		if (callback)
+			callback(panel.element);
+
+		tabs.activeOne(tab);
 	};
 
 	// 创建标签
 	function createTab(obj) {
 		if (tabParent) {
 			var temp = doc.createElement("div");
-			temp.innerHTML = templates.tab.replace(/\{text\}/, obj.text);
-			tabParent.appendChild(temp.children[0]);
+			var tpl = templates.tab.replace(/\{text\}/, obj.text).replace(/\{title\}/, obj.title ? obj.title : obj.text);
+			if (obj.closable === false)
+				tpl = tpl.replace(/\{\?.*\?\}/, "");
+			else
+				tpl = tpl.replace(/\{\?/, "").replace(/\?\}/, "");
+			temp.innerHTML = tpl;
+			return temp.children[0];
 		}
 	};
 
 	// 创建面板
 	function createPanel() {
 		if (panelParent) {
-
+			var temp = doc.createElement("div");
+			temp.innerHTML = templates.panel;
+			return temp.children[0];
 		}
 	};
 
 	// 设置标签的父元素
-	function setTabParent(tab) {
-		tabParent = getElement(tab);
+	function setTabParent(el) {
+		tabParent = getElement(el);
+	};
+
+	// 设置面板的父元素
+	function setPanelParent(el) {
+		panelParent = getElement(el);
 	};
 
 	function getElement(el) {
@@ -78,7 +205,8 @@ var panel = (function(win) {
 
 	return {
 		createNewPanel: createNewPanel,
-		setTabParent: setTabParent
+		setTabParent: setTabParent,
+		setPanelParent: setPanelParent
 	};
 
 }(window));
---
+++ zhoubao/public/javascripts/work.js
@@ -0,0 +1,8 @@
+tabPanelGroup.setTabParent("tabParent");
+tabPanelGroup.setPanelParent("panelParent");
+tabPanelGroup.createNewPanel({
+	id: 0,
+	text: "面板",
+	type: "main",
+	closable: false
+});
